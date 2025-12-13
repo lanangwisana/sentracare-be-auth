6f9db88ba06d22daf6dca61634439296df1f05a8
lanangwisana <lanangwisana@gmail.com>
2025-12-14 05:12:12 +0700

    Update code create new entity table users

--- models.py
+++ models.py
@@ -13,9 +13,12 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(100), nullable=False)
     username = Column(String(50), unique=True, index=True, nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
+    phone_number = Column(String(20), index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
     role = Column(Enum(RoleEnum), nullable=False, default=RoleEnum.PASIEN)
+    address = Column(String(255), nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
