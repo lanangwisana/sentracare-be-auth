d752705245127f769baa22d1fb4063f09f53cdc5
lanangwisana <lanangwisana@gmail.com>
2025-12-18 20:22:49 +0700

    Update code from schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,5 @@
 # SENTRACARE-BE-AUTH/schemas.py
+from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from typing import Literal
 
@@ -12,10 +13,19 @@ class RegisterRequest(BaseModel):
     address: str | None = None
 
 class AdminCreateUserRequest(BaseModel):
+    full_name: str
     username: str
     email: EmailStr
+    phone_number: str
     password: str
     role: Literal["Pasien", "Dokter", "SuperAdmin"]
+    status: Literal["Active", "Inactive"] = "Active"
+
+class AdminUpdateUserRequest(BaseModel):
+    full_name: str
+    username: str
+    email: EmailStr
+    status: Literal["Active", "Inactive"]
 
 class LoginRequest(BaseModel):
     identifier: str
@@ -29,6 +39,11 @@ class UserResponse(BaseModel):
     phone_number: str
     address: str | None = None
     role: Literal["Pasien", "Dokter", "SuperAdmin"]
+    status: Literal["Active", "Inactive"]
+    last_login: datetime | None = None
+    created_at: datetime
+    updated_at: datetime
+
 
     class Config:
         from_attributes = True 
